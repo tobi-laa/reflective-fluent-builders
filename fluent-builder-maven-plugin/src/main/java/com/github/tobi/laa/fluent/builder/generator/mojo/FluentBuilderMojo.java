4593dbdcfb7405461d68931a48ca0a876d227d48
Tobias Laatsch <tobias.laatsch@posteo.de>
2022-12-04 17:37:40 +0100

    Add ArchUnit tests

--- fluent-builder-maven-plugin/src/main/java/com/github/tobi/laa/fluent/builder/generator/mojo/FluentBuilderMojo.java
+++ fluent-builder-maven-plugin/src/main/java/com/github/tobi/laa/fluent/builder/generator/mojo/FluentBuilderMojo.java
@@ -1,4 +1,4 @@
-package com.github.tobi.laa.fluent.builder.maven.plugin.mojo;
+package com.github.tobi.laa.fluent.builder.generator.mojo;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
---
+++ fluent-builder-maven-plugin/src/test/java/com/github/tobi/laa/fluent/builder/archunit/ArchUnitTests.java
@@ -0,0 +1,28 @@
+package com.github.tobi.laa.fluent.builder.archunit;
+
+import com.github.tobi.laa.fluent.builder.ArchUnitMarker;
+import com.tngtech.archunit.core.importer.ImportOption;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.junit.ArchTests;
+
+/**
+ * <p>
+ * Main class for executing ArchUnit tests.
+ * </p>
+ */
+@AnalyzeClasses(packagesOf = ArchUnitMarker.class, importOptions = ImportOption.DoNotIncludeTests.class)
+class ArchUnitTests {
+
+    @ArchTest
+    private final ArchTests codingRules = ArchTests.in(CodingRules.class);
+
+    @ArchTest
+    private final ArchTests dependencyRules = ArchTests.in(DependencyRules.class);
+
+    @ArchTest
+    private final ArchTests interfaceRules = ArchTests.in(InterfaceRules.class);
+
+    @ArchTest
+    private final ArchTests namingConventionRules = ArchTests.in(NamingConventionRules.class);
+}
