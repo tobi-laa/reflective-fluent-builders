b639d2198a5f0d2b77d4894b1b6e145ae49ecc62
Tobias Laatsch <tobias.laatsch@posteo.de>
2023-02-10 22:53:33 +0100

    Add service for generating initializing statements for enum sets

---
+++ reflective-fluent-builders-generator/src/test/java/com/github/tobi/laa/reflective/fluent/builders/generator/service/impl/EnumSetInitializerCodeBlockGeneratorServiceImplTest.java
@@ -0,0 +1,129 @@
+package com.github.tobi.laa.reflective.fluent.builders.generator.service.impl;
+
+import com.github.tobi.laa.reflective.fluent.builders.generator.exception.CodeGenerationException;
+import com.github.tobi.laa.reflective.fluent.builders.generator.model.CollectionSetter;
+import com.github.tobi.laa.reflective.fluent.builders.generator.model.Visibility;
+import com.squareup.javapoet.CodeBlock;
+import org.assertj.core.api.ThrowableAssert;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class EnumSetInitializerCodeBlockGeneratorServiceImplTest {
+
+    private final EnumSetInitializerCodeBlockerGeneratorServiceImpl enumSetInitializerCodeBlockerGeneratorServiceImpl = new EnumSetInitializerCodeBlockerGeneratorServiceImpl();
+
+    @Test
+    void isApplicableNull() {
+        // Act
+        final Executable isApplicable = () -> enumSetInitializerCodeBlockerGeneratorServiceImpl.isApplicable(null);
+        // Assert
+        assertThrows(NullPointerException.class, isApplicable);
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void isApplicableTrue(final CollectionSetter collectionSetter) {
+        // Act
+        final boolean isApplicable = enumSetInitializerCodeBlockerGeneratorServiceImpl.isApplicable(collectionSetter);
+        // Assert
+        assertTrue(isApplicable);
+    }
+
+    private static Stream<CollectionSetter> isApplicableTrue() {
+        return generateCollectionInitializer().map(args -> args.get()[0]).map(CollectionSetter.class::cast);
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void isApplicableFalse(final CollectionSetter collectionSetter) {
+        // Act
+        final boolean isApplicable = enumSetInitializerCodeBlockerGeneratorServiceImpl.isApplicable(collectionSetter);
+        // Assert
+        assertFalse(isApplicable);
+    }
+
+    private static Stream<CollectionSetter> isApplicableFalse() {
+        return Stream.concat(
+                        SimpleCollectionInitializerCodeBlockGeneratorServiceImpl.SUPPORTED_COLLECTIONS.stream(),
+                        Stream.of(Set.class, List.class, SimpleCollectionInitializerCodeBlockGeneratorServiceImplTest.MyList.class))
+                .map(type -> CollectionSetter.builder() //
+                        .paramName("") //
+                        .paramType(type) //
+                        .paramTypeArg(Object.class) //
+                        .methodName("") //
+                        .visibility(Visibility.PRIVATE) //
+                        .build());
+    }
+
+    @Test
+    void generateCollectionInitializerNull() {
+        // Act
+        final Executable generateCollectionInitializer = () -> enumSetInitializerCodeBlockerGeneratorServiceImpl.generateCollectionInitializer(null);
+        // Assert
+        assertThrows(NullPointerException.class, generateCollectionInitializer);
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void generateCollectionInitializerCodeGenerationException(final CollectionSetter collectionSetter) {
+        // Act
+        final ThrowableAssert.ThrowingCallable generateCollectionInitializer = () -> enumSetInitializerCodeBlockerGeneratorServiceImpl.generateCollectionInitializer(collectionSetter);
+        // Assert
+        assertThatThrownBy(generateCollectionInitializer)
+                .isInstanceOf(CodeGenerationException.class)
+                .message()
+                .matches("Generation of initializing code blocks for .+ is not supported.")
+                .contains(collectionSetter.getParamType().getName());
+    }
+
+    private static Stream<CollectionSetter> generateCollectionInitializerCodeGenerationException() {
+        return isApplicableFalse();
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void generateCollectionInitializer(final CollectionSetter collectionSetter, final CodeBlock expected) {
+        // Act
+        final CodeBlock actual = enumSetInitializerCodeBlockerGeneratorServiceImpl.generateCollectionInitializer(collectionSetter);
+        // Assert
+        assertThat(actual.toString()).isEqualTo(expected.toString());
+    }
+
+    private static Stream<Arguments> generateCollectionInitializer() {
+        return Stream.of(
+                Arguments.of(
+                        CollectionSetter.builder() //
+                                .paramName("") //
+                                .paramType(EnumSet.class) //
+                                .paramTypeArg(EnumA.class) //
+                                .methodName("") //
+                                .visibility(Visibility.PRIVATE) //
+                                .build(),
+                        CodeBlock.builder().add("java.util.EnumSet.noneOf(com.github.tobi.laa.reflective.fluent.builders.generator.service.impl.EnumSetInitializerCodeBlockGeneratorServiceImplTest.EnumA.class)").build()),
+                Arguments.of(
+                        CollectionSetter.builder() //
+                                .paramName("") //
+                                .paramType(EnumSet.class) //
+                                .paramTypeArg(EnumB.class) //
+                                .methodName("") //
+                                .visibility(Visibility.PRIVATE) //
+                                .build(),
+                        CodeBlock.builder().add("java.util.EnumSet.noneOf(com.github.tobi.laa.reflective.fluent.builders.generator.service.impl.EnumSetInitializerCodeBlockGeneratorServiceImplTest.EnumB.class)").build()));
+    }
+
+    private static enum EnumA {}
+
+    private static enum EnumB {}
+}
