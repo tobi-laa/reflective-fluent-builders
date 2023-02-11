d1a79bd9a1469786a13d0759a3a6f89f9ef684ee
Tobias Laatsch <tobias.laatsch@posteo.de>
2023-02-11 14:33:01 +0100

    Refactoring

--- reflective-fluent-builders-generator/src/main/java/com/github/tobi/laa/reflective/fluent/builders/constants/BuilderConstants.java
+++ reflective-fluent-builders-generator/src/main/java/com/github/tobi/laa/reflective/fluent/builders/constants/BuilderConstants.java
@@ -3,10 +3,6 @@ package com.github.tobi.laa.reflective.fluent.builders.constants;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-
 /**
  * <p>
  * Holds constants used by multiples services, configuration or mojos.
@@ -16,8 +12,4 @@ import java.util.Set;
 public final class BuilderConstants {
 
     public static final String PACKAGE_PLACEHOLDER = "<PACKAGE_NAME>";
-
-    private static final Set<Class<?>> SUPPORTED_MAPS = Set.of( //
-            Map.class, HashMap.class
-    );
 }
