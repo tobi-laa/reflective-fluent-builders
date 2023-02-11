4b39a9d5d27634c3966bae82304076ef3622fd57
Tobias Laatsch <tobias.laatsch@posteo.de>
2023-02-11 16:13:49 +0100

    Helping model classes to be used by generators as return value

---
+++ reflective-fluent-builders-generator/src/main/java/com/github/tobi/laa/reflective/fluent/builders/generator/model/CollectionClassSpec.java
@@ -0,0 +1,23 @@
+package com.github.tobi.laa.reflective.fluent.builders.generator.model;
+
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeSpec;
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * <p>
+ * Holds information about an inner class which provides convenience methods for fluently adding elements to a
+ * collection, a map or an array.
+ * </p>
+ */
+@Data
+@Builder
+public class CollectionClassSpec {
+
+    @lombok.NonNull
+    private final MethodSpec getter;
+
+    @lombok.NonNull
+    private final TypeSpec innerClass;
+}
