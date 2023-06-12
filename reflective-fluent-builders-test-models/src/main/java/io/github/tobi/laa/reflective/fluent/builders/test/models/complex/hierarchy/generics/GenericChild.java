baf3c5fc000355714734f9440177e3e18f62a3a8
Tobias Laatsch <tobias.laatsch@posteo.de>
2023-06-12 21:36:54 +0200

    #106 Add new test models

---
+++ reflective-fluent-builders-test-models/src/main/java/io/github/tobi/laa/reflective/fluent/builders/test/models/complex/hierarchy/generics/GenericChild.java
@@ -0,0 +1,22 @@
+package io.github.tobi.laa.reflective.fluent.builders.test.models.complex.hierarchy.generics;
+
+import java.util.List;
+import java.util.Map;
+
+public class GenericChild<S extends Number, T> extends GenericParent<String, S, T> {
+
+    @Override
+    public void setList(List<String> list) {
+        super.setList(list);
+    }
+
+    @Override
+    public void setMap(Map<S, T> map) {
+        super.setMap(map);
+    }
+
+    @Override
+    public void setGeneric(Generic<T> generic) {
+        super.setGeneric(generic);
+    }
+}
