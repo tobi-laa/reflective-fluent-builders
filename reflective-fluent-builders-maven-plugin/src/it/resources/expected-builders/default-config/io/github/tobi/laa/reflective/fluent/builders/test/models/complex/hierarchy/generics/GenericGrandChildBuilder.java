9c8d389cf67f915a80e9326c9a627f4a81ae1ddd
Tobias Laatsch <tobias.laatsch@posteo.de>
2023-06-17 01:10:14 +0200

    Regenerate expected builder for Java 8

--- reflective-fluent-builders-maven-plugin/src/it/resources/expected-builders/default-config/io/github/tobi/laa/reflective/fluent/builders/test/models/complex/hierarchy/generics/GenericGrandChildBuilder.java
+++ reflective-fluent-builders-maven-plugin/src/it/resources/expected-builders/default-config/io/github/tobi/laa/reflective/fluent/builders/test/models/complex/hierarchy/generics/GenericGrandChildBuilder.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import javax.annotation.processing.Generated;
+import javax.annotation.Generated;
 
 @Generated(
     value = "io.github.tobi.laa.reflective.fluent.builders.generator.api.JavaFileGenerator",
