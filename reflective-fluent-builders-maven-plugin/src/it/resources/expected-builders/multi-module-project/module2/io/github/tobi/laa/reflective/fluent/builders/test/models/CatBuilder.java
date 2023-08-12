7bfc557dad34838189cea504f866ee940abaae4e
Tobias Laatsch <tobias.laatsch@posteo.de>
2023-08-12 19:29:22 +0200

    #136 IT

---
+++ reflective-fluent-builders-maven-plugin/src/it/resources/expected-builders/multi-module-project/module2/io/github/tobi/laa/reflective/fluent/builders/test/models/CatBuilder.java
@@ -0,0 +1,59 @@
+package io.github.tobi.laa.reflective.fluent.builders.test.models;
+
+import java.lang.String;
+import java.util.Objects;
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "io.github.tobi.laa.reflective.fluent.builders.generator.api.JavaFileGenerator",
+    date = "3333-03-13T00:00Z[UTC]"
+)
+public class CatBuilder {
+  /**
+   * This field is solely used to be able to detect generated builders via reflection at a later stage.
+   */
+  private boolean ______generatedByReflectiveFluentBuildersGenerator;
+
+  private Cat objectToBuild;
+
+  private final CallSetterFor callSetterFor = new CallSetterFor();
+
+  private final FieldValue fieldValue = new FieldValue();
+
+  private CatBuilder(final Cat objectToBuild) {
+    this.objectToBuild = objectToBuild;
+  }
+
+  public static CatBuilder newInstance() {
+    return new CatBuilder(null);
+  }
+
+  public static CatBuilder thatModifies(final Cat objectToModify) {
+    Objects.requireNonNull(objectToModify);
+    return new CatBuilder(objectToModify);
+  }
+
+  public CatBuilder fur(final String fur) {
+    fieldValue.fur = fur;
+    callSetterFor.fur = true;
+    return this;
+  }
+
+  public Cat build() {
+    if (objectToBuild == null) {
+      objectToBuild = new Cat();
+    }
+    if (callSetterFor.fur) {
+      objectToBuild.setFur(fieldValue.fur);
+    }
+    return objectToBuild;
+  }
+
+  private class CallSetterFor {
+    boolean fur;
+  }
+
+  private class FieldValue {
+    String fur;
+  }
+}
---
+++ reflective-fluent-builders-maven-plugin/src/it/resources/io/github/tobi/laa/reflective/fluent/builders/mojo/GenerateBuildersMojoIT/MultiModuleBuild/multiModuleBuild/module2/src/main/java/io/github/tobi/laa/reflective/fluent/builders/test/models/Cat.java
@@ -0,0 +1,10 @@
+package io.github.tobi.laa.reflective.fluent.builders.test.models;
+
+class Cat {
+
+    private String fur;
+
+    public void setFur(final String fur) {
+        this.fur = fur;
+    }
+}
