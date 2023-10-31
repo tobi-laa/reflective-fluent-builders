cbfbe0d88752bb7298ba779c7c6d7b6f58214271
Tobias Laatsch <tobias.laatsch@posteo.de>
2023-11-01 00:55:09 +0100

    Extend JavaClass model

--- reflective-fluent-builders-generator/src/main/java/io/github/tobi/laa/reflective/fluent/builders/model/JavaClass.java
+++ reflective-fluent-builders-generator/src/main/java/io/github/tobi/laa/reflective/fluent/builders/model/JavaClass.java
@@ -20,6 +20,13 @@ public class JavaClass {
     @lombok.NonNull
     private final Class<?> clazz;
 
+    @lombok.NonNull
+    private final Visibility visibility;
+
+    private final boolean isAbstract;
+
+    private final boolean isStatic;
+
     private final Path classLocation;
 
     private final Path sourceLocation;
@@ -50,5 +57,65 @@ public class JavaClass {
      */
     public Optional<Path> getSourceLocation() {
         return Optional.ofNullable(classLocation);
+    }
+
+    /**
+     * <p>
+     * Determines if this {@code JavaClass} object represents an interface type.
+     * </p>
+     *
+     * @return {@code true} if this {@code JavaClass} object represents an interface; {@code false} otherwise.
+     * @see Class#isInterface()
+     */
+    public boolean isInterface() {
+        return clazz.isInterface();
+    }
+
+    /**
+     * <p>
+     * Returns {@code true} if the underlying class is an anonymous class.
+     * </p>
+     *
+     * @return {@code true} if this class is an anonymous class.
+     * @see Class#isAnonymousClass()
+     */
+    public boolean isAnonymousClass() {
+        return clazz.isAnonymousClass();
+    }
+
+    /**
+     * <p>
+     * Returns true if this class was declared as an enum in the source code.
+     * </p>
+     *
+     * @return true if this class was declared as an enum in the source code
+     * @see Class#isEnum()
+     */
+    public boolean isEnum() {
+        return clazz.isEnum();
+    }
+
+    /**
+     * <p>
+     * Determines if this {@code JavaClass} object represents a primitive type.
+     * </p>
+     *
+     * @return true if this class represents a primitive type
+     * @see Class#isPrimitive()
+     */
+    public boolean isPrimitive() {
+        return clazz.isPrimitive();
+    }
+
+    /**
+     * <p>
+     * Returns {@code true} if the underlying class is a member class.
+     * </p>
+     *
+     * @return {@code true} if this class is a member class.
+     * @see Class#isMemberClass()
+     */
+    public boolean isMemberClass() {
+        return clazz.isMemberClass();
     }
 }
