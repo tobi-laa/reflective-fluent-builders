414562cc53f16775ba4c6138b3e59559d8be2051
Tobias Laatsch <tobias.laatsch@posteo.de>
2023-11-01 00:30:56 +0100

    New model for methods

---
+++ reflective-fluent-builders-generator/src/main/java/io/github/tobi/laa/reflective/fluent/builders/model/JavaMethod.java
@@ -0,0 +1,49 @@
+package io.github.tobi.laa.reflective.fluent.builders.model;
+
+import java.lang.reflect.Method;
+
+/**
+ * <p>
+ * Represents a Java {@link java.lang.reflect.Method method}. Contains the {@link java.lang.reflect.Method} object as
+ * well as additional metadata.
+ * </p>
+ */
+public interface JavaMethod {
+
+    /**
+     * <p>
+     * The underlying {@link Method}.
+     * </p>
+     *
+     * @return The underlying method.
+     */
+    Method getMethod();
+
+    /**
+     * <p>
+     * The name of the method, for instance {@code setAge}.
+     * </p>
+     *
+     * @return The name of the method.
+     */
+    String getMethodName();
+
+    /**
+     * <p>
+     * The visibility of the method, for instance {@code PUBLIC}.
+     * </p>
+     *
+     * @return The visibility of the method.
+     */
+    Visibility getVisibility();
+
+    /**
+     * <p>
+     * The class within which this method is defined. This is particularly important for methods inherited from super
+     * classes or interfaces.
+     * </p>
+     *
+     * @return The class within which this method is defined.
+     */
+    JavaClass getDeclaringClass();
+}
